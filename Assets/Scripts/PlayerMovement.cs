dd8112ca7b97e32abd021c95190405c8c4fbb8f5
alyssamt <alyssamt@uci.edu>
2016-10-14 20:36:14 -0700

    Base setup
    Player can move and collide with MC Hammer

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
