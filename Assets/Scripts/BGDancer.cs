37b364b14f8d73200cded47e28314c7803ad8495
Rikerd <cursedmarkedsasuke@gmail.com>
2016-10-14 21:46:26 -0700

    Circle Movement

---
+++ Assets/Scripts/BGDancer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGDancer : MonoBehaviour
+{
+
+    public Transform center;
+    public float degreesPerSecond = -65.0f;
+
+    private Vector3 v;
+
+    // Use this for initialization
+    void Start()
+    {
+        v = transform.position - center.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Quaternion.AngleAxis(degreesPerSecond * Time.deltaTime, Vector3.forward) * v;
+        transform.position = center.position + v;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("YOU LOSE");
+    }
+}
