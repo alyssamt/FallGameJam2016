8da315bc104bee7f3849f2631db5ea1e085be28e
alyssamt <alyssamt@uci.edu>
2016-10-15 22:34:48 -0700

    Main Menu finished

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
 
+    GameObject MenuPage;
+    GameObject CreditsPage;
+
 	// Use this for initialization
 	void Start () {
-	
+        MenuPage = GameObject.Find("MenuPage");
+        CreditsPage = GameObject.Find("CreditsPage");
+        CreditsPage.SetActive(false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void CreditsButton()
+    {
+        CreditsPage.SetActive(true);
+        MenuPage.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        MenuPage.SetActive(true);
+        CreditsPage.SetActive(false);
+    }
+
 }
