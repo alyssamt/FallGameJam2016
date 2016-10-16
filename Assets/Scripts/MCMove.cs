2f369a5c6113d47d1a130f162b7664e160d35d8d
metrognome1 <onceadayeveryday@gmail.com>
2016-10-16 10:39:35 -0700

    Fixed MC move
    Made mc move hit boundaries correctly

--- Assets/Scripts/MCMove.cs
+++ Assets/Scripts/MCMove.cs
@@ -6,12 +6,12 @@ using System.Collections;
 public class MCMove : MonoBehaviour
 {
 
-    public float maxX = 8F;
-    public float minX = -8F;
+
     public int moveSpeed = 5;
 
-    public float tChange = 0;
-    public float randomX = 0;
+    private float tChange = 0;
+    private float randomX = 0;
+    private float distance = 0;
 
     public bool active = false;
 
@@ -26,30 +26,10 @@ public class MCMove : MonoBehaviour
     {
         if (active)
         {
-            // change to random direction at random intervals
-            if (Time.time >= tChange)
-            {
-                randomX = Random.Range(0.0F, 1.0F);
-                // set a random interval between 0.7 and 0.9
-                tChange = Time.time + Random.Range(0.7F, 0.9F);
-            }
-            if (randomX <= 0.5F)
-            {
-                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            }
-            else if (randomX > 0.5F)
-            {
-                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-            }// if object reached any border, revert the appropriate direction
-            if (transform.position.x >= maxX || transform.position.x <= minX)
-            {
-                randomX = 1 - randomX;
-            }
-
-            Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-            pos.x = Mathf.Clamp(pos.x, 0.0F, 1.0F);
-            transform.position = Camera.main.ViewportToWorldPoint(pos);
+            transform.position = new Vector2(Mathf.PingPong(Time.time * 3, (2 * max.x)) - max.x, Mathf.PingPong(Time.time * 3, (2 * max.y)) - max.y);
         }
     }
 }
